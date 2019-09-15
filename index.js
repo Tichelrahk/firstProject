fb3b3eed21dded84069e48981941c4920f05bfe9
Etienne Bovet <ebovet@gmail.com>
2019-09-15 16:12:04 -0700

    prep for Heroku

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const http = require('http'); //import library for requests
-
-const hostname = '127.0.0.1'; //the ip address
-
-const port = 3000; //this is the port
-
-const server = http.createServer((req, res) => { //set up the server
+const http = require('http');
+const PORT = process.env.PORT || 5000;
+const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 });
-
-server.listen(port, hostname, () => { //listen at above constants
-  console.log(`Server running at http://${hostname}:${port}/`); //print so I know it's working. The curly cues and $ allow us to pass the variables into the string
+server.listen(PORT, () => {
+  console.log(`Server running on ${PORT}/`);
 });
