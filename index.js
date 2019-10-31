5c4f2e16888f7f59eb17de0089b56bee9846af6a
Etienne Bovet <ebovet@gmail.com>
2019-10-31 09:48:24 +0800

    Trying things

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('briefcase.png');
+  res.end(
+  	<h1>Hello</h1>
+  	);
 });
 server.listen(PORT, () => {
   console.log(`Server running on ${PORT}/`);
