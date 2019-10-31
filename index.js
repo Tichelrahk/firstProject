034a695a08c5f441eefe9caa8c79d68fbabc8d1f
Etienne Bovet <ebovet@gmail.com>
2019-10-31 10:56:16 +0800

    First push of html file

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+const fs = require('fs');
 const http = require('http');
 const PORT = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end("Hello");
+  fs.readFile('homepageview.html', function(err, data) {
+	  res.setHeader('Content-Type', 'text/plain');
+	  res.write(data);
+	  res.end();
+	});
 });
 server.listen(PORT, () => {
   console.log(`Server running on ${PORT}/`);
