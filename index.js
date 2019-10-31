4748ec5fbf70e3c463361009a1845a4e0df6af58
Etienne Bovet <ebovet@gmail.com>
2019-10-31 11:07:30 +0800

    Still trying

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const fs = require('fs');
 const http = require('http');
 const PORT = process.env.PORT || 5000;
+var fs = require('fs');
 const server = http.createServer((req, res) => {
+  res.statusCode = 200;
   fs.readFile('homepageview.html', function(err, data) {
 	  res.writeHead(200, {'Content-Type': 'text/html'});
 	  res.write(data);
