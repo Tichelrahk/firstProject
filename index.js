524bb9d7b7b5c3e8bc01ef0aff8797aa7242a19e
Etienne Bovet <ebovet@gmail.com>
2019-10-31 11:10:06 +0800

    Still trying

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const http = require('http');
 const PORT = process.env.PORT || 5000;
-var fs = require('fs');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  fs.readFile('homepageview.html', function(err, data) {
-	  res.writeHead(200, {'Content-Type': 'text/html'});
-	  res.write(data);
-	  res.end();
-	});
+  res.setHeader('Content-Type', 'text/plain');
+  res.end("Hello it doesn't work");
 });
 server.listen(PORT, () => {
   console.log(`Server running on ${PORT}/`);
