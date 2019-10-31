88132cde4106f6509905989c2495b57aa9c831af
Etienne Bovet <ebovet@gmail.com>
2019-10-31 12:07:55 +0800

    adding my new files

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const http = require('http');
 const PORT = process.env.PORT || 5000;
+const fs = require('fs');
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end("Hello it doesn't work");
+	fs.readFile('homepageview.html', function(err, data) {
+	  res.writeHead(200, {'Content-Type': 'text/html'});
+	  res.write(data);
+	  res.end()});
 });
 server.listen(PORT, () => {
   console.log(`Server running on ${PORT}/`);
